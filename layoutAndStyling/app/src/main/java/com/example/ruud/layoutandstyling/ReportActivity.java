83b50663b893ba174577ae0a2b40c06c589eb10c
RuudV <ruud.vissers@student.fontys.nl>
2018-02-22 11:28:41 +0100

    finished layout
    I finished the layout assignment

--- layoutAndStyling/app/src/main/java/com/example/ruud/layoutandstyling/ReportActivity.java
+++ layoutAndStyling/app/src/main/java/com/example/ruud/layoutandstyling/ReportActivity.java
@@ -6,17 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ReportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button RepoButton = findViewById(R.id.repoButton);
-        RepoButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_report);
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-                startActivity(new Intent(MainActivity.this, ReportActivity.class));
+                startActivity(new Intent(ReportActivity.this, MainActivity.class));
             }
         });
     }

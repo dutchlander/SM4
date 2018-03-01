463f9101335b26790d36fc0308ee7d9d8a282dcd
RuudV <ruud.vissers@student.fontys.nl>
2018-03-01 12:28:12 +0100

    Persistance app
    tutorial gevolgd voor het schrijven en lezen van en uit een file.

---
+++ Persistance/app/src/main/java/com/example/ruud/persistance/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.ruud.persistance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static android.view.View.INVISIBLE;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.edittext);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setVisibility(INVISIBLE);
+    }
+
+    public void writeMessage(View view)
+    {
+        String Message = editText.getText().toString();
+        String fileName = "hello_file";
+        try
+        {
+            FileOutputStream fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
+            fileOutputStream.write(Message.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getApplicationContext(), "Message saved", Toast.LENGTH_LONG).show();
+            editText.setText("");
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void readMessage(View view)
+    {
+        try {
+            String Message;
+            FileInputStream fileInputStream = openFileInput("hello_file");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            while((Message=bufferedReader.readLine()) != null)
+            {
+                stringBuffer.append(Message +"\n");
+            }
+            textView.setText(stringBuffer.toString());
+            textView.setVisibility(View.VISIBLE);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
